1735d8057a04b290e2065bd5dd150d2ab83bfac1
Lutmila <lutmilavalles05@gmail.com>
2023-06-01 22:53:57 -0300

    realizado

---
+++ eje4-tp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int segundos;
+            int numero;
+            int minutos;
+            int horas;
+
+
+            Console.WriteLine("Ingrese un numero total de segundo");
+            segundos =int.Parse (Console.ReadLine());
+
+
+
+
+
+        }
+
+
+    }
+}

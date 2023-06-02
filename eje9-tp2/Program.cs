954246bc0b2cbf4ffc0b7d0d5c6315b1a105f737
Lutmila <lutmilavalles05@gmail.com>
2023-06-01 23:11:46 -0300

    realizado

---
+++ eje9-tp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nº9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double  ventas,sueldo;
+
+            Console.WriteLine("ingrese su sueldo :");
+            sueldo = double.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese cantidad de ventas :");
+            ventas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("La comision es : " + (sueldo * 0.10));
+            Console.ReadLine();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

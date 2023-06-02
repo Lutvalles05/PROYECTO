db82cffd3145780345d7a255e7ec1cae3cd07bad
Lutmila <lutmilavalles05@gmail.com>
2023-06-01 22:45:54 -0300

    realizado

---
+++ eje2-tp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nº2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peso;
+            int dolar;
+            int euro;
+
+            Console.WriteLine("ingrese la cantidad de pesos ");
+            peso = int.Parse(Console.ReadLine());
+
+
+            euro =  peso/ 227;
+            dolar =  peso/208;
+
+
+            Console.Write("tu cantidad en euro es" + dolar);
+            Console.WriteLine();
+            Console.Write("tu cantidad en dolar es " + euro) ;
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}

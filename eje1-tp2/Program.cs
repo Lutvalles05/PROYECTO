e9df0d5d5b48f3c03e689a624978107fa97b6cbe
Lutmila <lutmilavalles05@gmail.com>
2023-06-01 21:32:35 -0300

    realizado

---
+++ eje1-tp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nº1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            Console.WriteLine("¿cual es tu nombre? ");
+            nombre = Console.ReadLine();
+           
+            
+            Console.WriteLine("Hola,"+ nombre);
+
+            Console.ReadKey();
+
+        }
+    }
+}
